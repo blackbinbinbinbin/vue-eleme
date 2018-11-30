33d6d2bb190ffbb267c251459ecbc3380e4270f8
blackbinbin <blackbinbin@foxmail.com>
2018-11-30 08:47:10 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue/dist/vue.js';
+import './cube-ui';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Goods from 'components/goods/goods.vue';
+import Ratings from 'components/ratings/ratings.vue';
+import Seller from 'components/seller/seller.vue';
+// stylus
+import 'common/stylus/index.styl';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// router
+Vue.use(VueRouter);
+const routes = [
+  { path: '/goods', component: Goods },
+  { path: '/rating', component: Ratings },
+  { path: '/seller', component: Seller }
+]
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
